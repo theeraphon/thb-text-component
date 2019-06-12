5380f45649f3235ce1401960a94332e1a6f5f06a
Theeraphon Samathi <teeraphon.samathi@thomsonreuters.com>
2019-06-12 18:44:18 +0700

    complete converter

--- thb-text-component.js
+++ thb-text-component.js
@@ -27,17 +27,66 @@ class ThbTextComponent extends PolymerElement {
     };
   }
 
+
+
   constructor() {
     super();
+    this.numberText = ['ศูนย์', 'หนึ่ง', 'สอง', 'สาม', 'สี่', 'ห้า', 'หก', 'เจ็ด', 'แปด', 'เก้า'];
+    this.digitText = ['', 'สิบ', 'ร้อย', 'พัน', 'หมื่น', 'แสน'];
   }
 
   convertToText() {
-    // TODO: Add code to convert value to text here
-    const value = this.parseNumber(this.value);
-    if (value) {
 
+    let value = this.parseNumber(this.value);
+    let text = '';
+    if (value) {
+      const valueStr = value.toString();
+      const arrStr = valueStr.split('.', 2);
+      const decicaml = arrStr[0];
+
+      if (decicaml != '0') {
+        text = this.getText(decicaml);
+      }
+      text = `${text}บาท`;
+      const point = arrStr[1] ? Number.parseInt(arrStr[1]).toString() : undefined;
+      if(point) {
+        text = `${text}${this.getText(point)}สตางค์`;
+      } else {
+        text = `${text}ถ้วน`;
+      }
     }
-    return value ? this.value : 'Error';
+    return value ? text : 'Error';
+  }
+
+  getText(value) {
+    let digit = 0;
+    let text = '';
+    for (let i = value.length - 1; i >= 0; i--) {
+      const million = Math.floor(digit / 6);
+      let millionText = '';
+      if (digit % 6 === 0) {
+        for (let j = 0; j < million; j++) {
+          millionText = millionText + 'ล้าน';
+        }
+      }
+      if (value[i] === '1' && digit % 6 === 0 && digit !== value.length - 1) {
+        text = `เอ็ด${millionText}${text}`;
+      }
+      else {
+        const numVal = Number.parseInt(value[i]);
+        if (digit % 6 === 1 && numVal === 1) {
+          text = `สิบ${text}`;
+        }
+        else if (digit % 6 === 1 && numVal === 2) {
+          text = `ยี่สิบ${text}`;
+        }
+        else {
+          text = `${this.numberText[Number.parseInt(value[i])]}${this.digitText[digit % 6]}${millionText}${text}`;
+        }
+      }
+      digit++;
+    }
+    return text;
   }
 
   parseNumber(val) {
